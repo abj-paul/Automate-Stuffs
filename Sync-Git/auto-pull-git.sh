95a38e5c37b118fa5965d858fed98024bf7b168f
abj-paul <bsse1201@iit.du.ac.bd>
2022-12-13 07:41:09 +0600

    Added git credential in scripts

--- Sync-Git/auto-pull-git.sh
+++ Sync-Git/auto-pull-git.sh
@@ -1,12 +1,16 @@
 #!/bin/bash
 
+
 cd ~/books/
+git config credential.helper cache --global
 git pull
 
 cd ~/classworks/
+git config credential.helper cache --global
 git pull
 
 cd ~/Org-Roam-Files/
+git config credential.helper cache --global
 git pull
 
 cd ~
--- Sync-Git/auto-push-git.sh
+++ Sync-Git/auto-push-git.sh
@@ -1,21 +1,25 @@
 #!/bin/bash
 
+
 current="`date +'%Y-%m-%d %H:%M:%S'`"
 msg="Books pushed from university: $current"
 
 cd ~/books/
+git config credential.helper cache --global
 git add --all
 git commit -m "$msg" 
 git push
 
 msg="Classworks pushed from university: $current"
 cd ~/classworks/
+git config credential.helper cache --global
 git add --all
 git commit -m "$msg" 
 git push
 
 msg="Org Roam Files pushed from university: $current"
 cd ~/Org-Roam-Files/
+git config credential.helper cache --global
 git add --all
 git commit -m "$msg" 
 git push
